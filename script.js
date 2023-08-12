514d4d94a3a8f90eba8bc38a8207cb389e89179f
T <141818631+JenesuispasHPI@users.noreply.github.com>
2023-08-12 20:48:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,57 @@
- const timer = document.getElementById("timer");
+ const timeElement = document.getElementById('time');
         const startBtn = document.getElementById("startBtn");
         const repeatBtn = document.getElementById("repeatBtn");
-        let time = 0;
-        let interval;
+        const messageElement = document.getElementById('message');
+        let minutes = 0;
+        let seconds = 0;
+
+function updateTimer() {
+    timeElement.textContent = `${minutes}:${seconds.toString().padStart(2, '0')}`;
+}
+
+function showMessage(message) {
+    messageElement.textContent = message;
+}
+
+function startTimer() {
+    startBtn.disabled = true;
+    const interval = setInterval(() => {
+        seconds++;
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+            if (minutes % 5 === 0) {
+                showMessage(getRewardMessage(minutes));
+            }
+        }
+        updateTimer();
+        if (minutes === 25) {
+            clearInterval(interval);
+            showMessage('Vous avez travaillé 25 minutes. Félicitations, vous êtes l\'arbre qui ne ploie pas devant la tâche à abattre !');
+        }
+    }, 1000);
+}
+
 
         function formatTime(seconds) {
             const mins = Math.floor(seconds / 60).toString().padStart(2, "0");
             const secs = (seconds % 60).toString().padStart(2, "0");
             return `${mins}:${secs}`;
         }
+
+function getRewardMessage(minutes) {
+    switch (minutes) {
+        case 5:
+            return 'Vous pouvez y arriver';
+        case 10:
+            return 'C’est en plantant une graine qu’on sème le succès';
+        case 15:
+            return 'Vous gérez la fougère !';
+        case 20:
+            return 'Vous n’en pouvez plus, mais le roseau ne rompt pas face à la tempête';
+    }
+}
+
 
         repeatBtn.addEventListener("click", function () {
             clearInterval(interval);

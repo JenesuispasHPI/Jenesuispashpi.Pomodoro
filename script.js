0cb7f8002663eb9f56ab7c4078f27a41fbb2b216
T <141818631+JenesuispasHPI@users.noreply.github.com>
2023-08-12 20:33:08 +0200

    Update script.js

--- script.js
+++ script.js
@@ -35,13 +35,13 @@ function startTimer() {
 function getRewardMessage(minutes) {
     switch (minutes) {
         case 5:
-            return 'Vous pouvez y arriver';
+            return 'Vous pouvez y arriver !';
         case 10:
-            return 'C’est en plantant une graine qu’on sème le succès';
+            return 'C’est en plantant une graine qu’on sème le succès !';
         case 15:
             return 'Vous gérez la fougère !';
         case 20:
-            return 'Vous n’en pouvez plus, mais le roseau ne rompt pas face à la tempête';
+            return 'Vous n’en pouvez plus, mais le roseau ne rompt pas face à la tempête !';
     }
 }
 
